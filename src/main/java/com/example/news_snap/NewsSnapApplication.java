c128167acafc28e35bd2eac9d3a8095ef7b52634
chamm99 <alslalsl3125@gmail.com>
2024-07-15 23:40:34 +0900

    :sparkles: Feat: Initialize Project

---
+++ src/main/java/com/example/news_snap/NewsSnapApplication.java
@@ -0,0 +1,13 @@
+package com.example.news_snap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsSnapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsSnapApplication.class, args);
+	}
+
+}
