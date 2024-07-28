4d0f197bbb75bcf0adf2a5f3f930367bd864ad87
Limwngur <ovg07047@naver.com>
2024-07-28 22:53:07 +0900

    :lipstick: [STYLE] 헤드라인 뉴스 API 반환 타입 명시 (#3) - ApiResponse<?> -> ApiResponse<List<NewsResponseDto>>

--- src/main/java/com/example/news_snap/domain/news/controller/NewsController.java
+++ src/main/java/com/example/news_snap/domain/news/controller/NewsController.java
@@ -1,5 +1,6 @@
 package com.example.news_snap.domain.news.controller;
 
+import com.example.news_snap.domain.news.dto.NewsResponseDto;
 import com.example.news_snap.domain.news.service.NewsService;
 import com.example.news_snap.global.common.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -8,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/news")
@@ -19,7 +22,7 @@ public class NewsController {
 
     @Operation(summary = "헤드라인 뉴스 목록 조회", description = "Naver 경제 헤드라인 뉴스를 모두(10개) 조회합니다.")
     @GetMapping("/head-line")
-    public ApiResponse<?> getHeadLineNews() {
+    public ApiResponse<List<NewsResponseDto>> getHeadLineNews() {
         return ApiResponse.onSuccess(newsService.getHeadLineNews());
     }
 }
