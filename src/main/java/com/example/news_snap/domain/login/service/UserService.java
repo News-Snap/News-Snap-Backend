2c8151d65476a042239a6dfd602710d1656382f5
= <sjhyun0926@gmail.com>
2024-08-02 12:55:51 +0900

    [FEATURE] 비밀번호 암호화

--- src/main/java/com/example/news_snap/domain/login/service/UserService.java
+++ src/main/java/com/example/news_snap/domain/login/service/UserService.java
@@ -4,6 +4,7 @@ import com.example.news_snap.domain.login.entity.User;
 import com.example.news_snap.domain.login.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -24,8 +25,14 @@ public class UserService {
         final User userEntity = userRepository.save(user);
         return userEntity;
     }
-    public User getByCredentials(String email, String password) {
-        return userRepository.findByEmailAndPassword(email, password);
+    //matches 메서드 이용해 패스워드 같은지 확인
+    //-> salting 고려해 두 값 비교
+    public User getByCredentials(final String email, final String password, final PasswordEncoder encoder) {
+        final User user = userRepository.findByEmail(email);
+        if (user != null && encoder.matches(password, user.getPassword())) {
+            return user;
+        }
+        return null;
     }
 
 }
