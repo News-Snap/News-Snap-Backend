15d9b55bf1270396020abc2c6e40c822c70936a8
= <sjhyun0926@gmail.com>
2024-08-02 12:11:54 +0900

    [FEATURE] 서블릿 필터 구현 및 설정 (#5)

--- src/main/java/com/example/news_snap/domain/login/security/TokenProvider.java
+++ src/main/java/com/example/news_snap/domain/login/security/TokenProvider.java
@@ -4,6 +4,7 @@ import com.example.news_snap.domain.login.entity.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,9 @@ import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
+//subject는 String userId 입니당 Long 아님 주의!!!
+//@AuthentificationPrincipal String userId
+//위에 어노테이션 컨트롤러 메서드 파라미터 마다 넣어주기
 @Slf4j
 @Service
 public class TokenProvider {
@@ -32,6 +36,31 @@ public class TokenProvider {
                 .compact();
     }
 
+
+    //토큰 유효시간 검사 추가
+    public String validateToken(String token) {
+        try {
+            Claims claims = Jwts.parser()
+                    .setSigningKey(SECRET_KEY)
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            // 토큰이 만료되었는지 확인 (추가)
+            if (claims.getExpiration().before(new Date())) {
+                throw new RuntimeException("Token expired");
+            }
+
+            return claims.getSubject();
+        } catch (SignatureException ex) {
+            log.error("Invalid JWT signature", ex);
+            throw new RuntimeException("Invalid JWT signature");
+        } catch (Exception ex) {
+            log.error("Invalid JWT token", ex);
+            throw new RuntimeException("Invalid JWT token");
+        }
+    }
+}
+/*
     public String validateToken(String token) {
 
         Claims claims = Jwts.parser()
@@ -41,3 +70,4 @@ public class TokenProvider {
         return claims.getSubject();
     }
 }
+*/
