3dd370086a6252908140cdbc4ddbf84156fd3377
Dlrjswn <lkj6387@gmail.com>
2024-08-17 14:19:47 +0900

    setting5

--- src/main/java/com/example/news_snap/domain/setting/controller/SettingController.java
+++ src/main/java/com/example/news_snap/domain/setting/controller/SettingController.java
@@ -19,31 +19,31 @@ public class SettingController {
 
     @Operation(summary = "설정사항 조회", description = "설정사항을 조회합니다.")
     @GetMapping("")
-    public ApiResponse<SettingResponse.settingDTO> getSetting(@PathVariable Long userId) {
+    public ApiResponse<SettingResponse.settingDTO> getSetting(@PathVariable(name="userId") Long userId) {
         return ApiResponse.onSuccess(settingService.getSetting(userId));
     }
 
     @Operation(summary = "닉네임 수정", description = "닉네임을 수정합니다.")
     @PatchMapping("/nickname")
-    public ApiResponse<SettingResponse.updateNicknameResultDTO> updateNickname(@PathVariable Long userId, @RequestBody SettingRequest.updateNicknameDTO request){
+    public ApiResponse<SettingResponse.updateNicknameResultDTO> updateNickname(@PathVariable(name="userId") Long userId, @RequestBody SettingRequest.updateNicknameDTO request){
         return ApiResponse.onSuccess(settingService.updateNickname(userId, request));
     }
 
     @Operation(summary = "푸쉬알람 여부 수정", description = "푸쉬알람 여부를 수정합니다.")
     @PatchMapping("/alarm")
-    public ApiResponse<SettingResponse.updatePushAlarmResultDTO> updatePush(@PathVariable Long userId, @RequestBody SettingRequest.updatePushAlarmDTO request){
+    public ApiResponse<SettingResponse.updatePushAlarmResultDTO> updatePush(@PathVariable(name="userId") Long userId, @RequestBody SettingRequest.updatePushAlarmDTO request){
         return ApiResponse.onSuccess(settingService.updatePushAlarm(userId, request));
     }
 
     @Operation(summary = "알람요일 수정", description = "알람요일을 수정합니다.")
     @PatchMapping("/day")
-    public ApiResponse<SettingResponse.updateAlarmDayResultDTO> updateDay(@PathVariable Long userId, @RequestBody SettingRequest.updateAlarmDayDTO request){
+    public ApiResponse<SettingResponse.updateAlarmDayResultDTO> updateDay(@PathVariable(name="userId") Long userId, @RequestBody SettingRequest.updateAlarmDayDTO request){
         return ApiResponse.onSuccess(settingService.updateAlarmDay(userId, request));
     }
 
     @Operation(summary = "알람시간 수정", description = "알람시간을 수정합니다.")
     @PatchMapping("/time")
-    public ApiResponse<SettingResponse.updateAlarmTimeResultDTO> updateTime(@PathVariable Long userId, @RequestBody SettingRequest.updateAlarmTimeDTO request){
+    public ApiResponse<SettingResponse.updateAlarmTimeResultDTO> updateTime(@PathVariable(name="userId") Long userId, @RequestBody SettingRequest.updateAlarmTimeDTO request){
         return ApiResponse.onSuccess(settingService.updateAlarmTime(userId, request));
     }
 
